725b4411b3c896134accb831215d2337bb0552fc
shuvo sarkar <shuvosarkar.dev@gmail.com>
2025-06-09 17:01:21 +0530

    general

---
+++ ch3_data_type/characters.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char my_char = 65;
+    char my_char2 = 'B';
+    char my_text[] = "Hello";
+
+    printf("mychar: %c, %c\n", my_char, my_char2);
+    printf("myText: %s", my_text);
+    
+    return 0;
+}
